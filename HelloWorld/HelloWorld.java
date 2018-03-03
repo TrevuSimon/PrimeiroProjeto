2780c49670c2a7661eeaec586a0c8b6f39bbb6ed
Guilherme <guilherme1301703@fapi-pinhais.edu.br>
2018-03-03 16:29:48 -0300

    Tirando main do HelloWorld

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,7 +1,7 @@
 package HelloWorld;
 
 public class HelloWorld {
-    public static void main(String[] args){
-        System.out.println("HelloWorld!");
+    public void helloWorld(String[] args){
+        
     }
 }
