b1d2e336c3b853d7ed62c30fd7975d9c702abeb9
Guilherme <guilherme1301703@fapi-pinhais.edu.br>
2018-03-03 17:58:07 -0300

    bugfix - string para String

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -1,7 +1,7 @@
 package HelloWorld;
 
 public class Main{
-    public static void main(string[] args){
+    public static void main(String[] args){
         HelloWorld hw = new HelloWorld();
         hw.helloWorld();
     }
